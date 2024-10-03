146546a05a0581b88bee1e9cfc99ea9f1f279de3
Shalk000 <Shalk000@209-11>
2024-10-03 10:02:47 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("Введите число t: ");
+    int t = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число y: ");
+    int y = int.Parse(Console.ReadLine());
+    double S = 4.351 * Math.Pow(y, 3) + (2 * t) * (Math.Log10(t)) / (Math.Sqrt(2 * Math.Cos(y) + 4.351));
+    Console.WriteLine($"Число S = {S:F2}");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
